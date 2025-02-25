9e47b29f34ae07da5bbe088a6f561d09073c745d
AVINASH SUTHAR <avinashsuthar19hmh@gmail.com>
2025-02-25 12:31:55 +0530

    Typing area animation and keyboard

--- packages/ui/src/components/typingArea.tsx
+++ packages/ui/src/components/typingArea.tsx
@@ -7,26 +7,6 @@ import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 import { BackgroundBeamsWithCollision } from "./ui/BackgroudBeams";
 // ..
-AOS.init({
-  // Global settings:
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: "aos-init", // class applied after initialization
-  animatedClassName: "aos-animate", // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 500, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 990, // the delay on throttle used while scrolling the page (advanced)
-
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 50, // values from 0 to 3000, with step 50ms
-  duration: 900, // values from 0 to 3000, with step 50ms
-  easing: "ease", // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
-});
 
 const sampleText =
   "High know eye. These hand small know place, a plan group, keep we think because run may long if plan, child he have, even find since follow however.";
@@ -65,6 +45,10 @@ const TypingTest = () => {
         : 100
     );
   }, [userInput]); // Removed startTime from dependency array
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
   const handleKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
     setKey(e.key);
