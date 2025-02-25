14dd1fdad073b37e16f2ba0fe8787f2ae507dbcc
AVINASH SUTHAR <avinashsuthar19hmh@gmail.com>
2025-02-25 22:49:43 +0530

    done vercel

--- apps/main/next-sitemap.config.js
+++ apps/main/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://avinashsuthar.in", // Your domain
+  siteUrl: "https://typing.avinashsuthar.in", // Your domain
   generateRobotsTxt: true, // Generate robots.txt
   sitemapBaseFileName: "sitemap",
   generateIndexSitemap: false, // Disable index sitemap if you don't have multiple sitemaps
