1681c2be724fd7808a0df986cdfc2cd57441c5a0
AVINASH SUTHAR <avinashsuthar19hmh@gmail.com>
2025-03-02 21:52:12 +0530

    dynamic text

--- apps/main/app/about/page.tsx
+++ apps/main/app/about/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='h-screen w-full '>about page</div>
+    <div className='h-screen w-full '>about page done</div>
   )
 }
 
