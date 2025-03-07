765608a9a2bc51a885020c1af227f4fb2a0d12cb
AVINASH SUTHAR <avinashsuthar19hmh@gmail.com>
2025-03-07 15:21:34 +0530

    seo rebase

--- apps/main/app/about/page.tsx
+++ apps/main/app/about/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='h-screen w-full '>this is the the seo is done test</div>
+    <div className='h-screen w-full '>th test</div>
   )
 }
 
