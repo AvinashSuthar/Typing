9866182884f10f6b601bb55fd19fbcd52197a65b
AVINASH SUTHAR <avinashsuthar19hmh@gmail.com>
2025-03-14 20:04:28 +0530

    done

--- apps/main/app/about/page.tsx
+++ apps/main/app/about/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='h-screen w-full '>tyoping test</div>
+    <div className='h-screen w-full '>typing test</div>
   )
 }
 
