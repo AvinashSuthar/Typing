02d534e36da27cc20fd8abcd1e4e5c7e4b63740e
AVINASH SUTHAR <avinashsuthar19hmh@gmail.com>
2025-03-21 21:25:13 +0530

    Typing area animation and keyboard

--- apps/main/app/about/page.tsx
+++ apps/main/app/about/page.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='h-screen w-full '>typing  ko update krna h  </div>
+    <div className='h-screen w-full '>typing  ko ha  krna h  </div>
   )
 }
 
 export default page
+
